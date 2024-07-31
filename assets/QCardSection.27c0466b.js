1022bd9973943fc0939fe89dc381074cf29265dd
MrWlker <150204038+MrWlker@users.noreply.github.com>
2024-07-31 08:37:43 -0300

    Delete assets directory

--- assets/QCardSection.27c0466b.js
+++
@@ -1,1 +0,0 @@
-import{k as r,c as e,h as n}from"./index.054d5cb1.js";import{a as s}from"./dom.bd9f1d87.js";var l=r({name:"QCardSection",props:{tag:{type:String,default:"div"},horizontal:Boolean},setup(t,{slots:a}){const o=e(()=>`q-card__section q-card__section--${t.horizontal===!0?"horiz row no-wrap":"vert"}`);return()=>n(t.tag,{class:o.value},s(a.default))}});export{l as Q};
