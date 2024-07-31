5998d9c69206c07730d2150b495482f5c91605f1
MrWlker <150204038+MrWlker@users.noreply.github.com>
2024-07-31 08:34:28 -0300

    versão 0.0.1

---
+++ versão 0.0.1/assets/QCardSection.3d8a01d5.js
@@ -0,0 +1,1 @@
+import{k as r,c as e,h as n}from"./index.5cf871b0.js";import{a as s}from"./dom.bf81c507.js";var l=r({name:"QCardSection",props:{tag:{type:String,default:"div"},horizontal:Boolean},setup(t,{slots:a}){const o=e(()=>`q-card__section q-card__section--${t.horizontal===!0?"horiz row no-wrap":"vert"}`);return()=>n(t.tag,{class:o.value},s(a.default))}});export{l as Q};
